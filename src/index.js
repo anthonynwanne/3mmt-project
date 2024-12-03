d21e08fffca636c8fa6f9e6d8d0994201f50207c
automateservicesllc <automateservicesllc@mail.com>
2024-12-03 15:23:10 +0100

    fixed10

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.get('/', (req, res) => {
-    res.find('/');
-});
+//app.get('/', (req, res) => {
+   // res.find('/');
+//});
 
 app.get('/', (req, res) => {
     res.render('login'); // Ensure "login" matches the filename in your views directory
