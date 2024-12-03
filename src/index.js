33c76c660d1068b267e2f01c5feb593e450eda5a
automateservicesllc <automateservicesllc@mail.com>
2024-12-03 21:08:49 +0100

    build4

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(methodOverride("_method"));
 // Use EJS as the view engine
 app.set('view engine', 'pug');
 
-app.set('views', path.join(__dirname, '~/'));
+//app.set('views', path.join(__dirname, '~/'));
 
 app.get('/', (req, res) => {
   res.render("login"); // Ensure "login" matches the filename in your views directory
